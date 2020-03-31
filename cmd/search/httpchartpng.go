openshift/ci-search

commit 06c45ec87ffe9467653b3fe15ac5c21e531d5e17

Switch to indexing jobs based on a list of input prow jobs
Maintain an informer that periodically polls deck for all jobs, then
notifies an extract worker to retrieve jobs that show up. The directory
on disk is set to the start timestamp until extraction completes and
then is set to the prow job completion timestamp so duplicate jobs will
be ignored. This will not capture historical records and a future change
should monitor that.

Add a hard cap on the number of paths in the index to avoid "argument
list too long" errors. Move some logic out of the index and into the
top level argument construction so that we can fall back to searching
everything.

Start using the .Done() method on queues - that was an oversight and
would lead to unbounded memory growth in some cases.

cmd/search/jobs.go
@@ -5,51 +5,67 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
+	"github.com/openshift/ci-search/prow"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/klog"
 )
 
 var jobLock sync.Mutex
 var jobBytes []byte
 
+type Job prow.Job
+
+func (job *Job) StartStop() (time.Time, time.Time, error) {
+	return time.Time{}, time.Time{}, nil
+	// var zero time.Time
+
+	// started, err := time.Parse(time.RFC3339, job.Status.StartTime)
+	// if err != nil {
+	// 	return zero, zero, fmt.Errorf("prow job %s #%s had invalid 'startTime': %s", job.Spec.Job, job.Status.BuildID, err)
+	// }
+
+	// var finished time.Time
+	// if job.Status.CompletionTime != "" {
+	// 	finished, err = time.Parse(time.RFC3339, job.Status.CompletionTime)
+	// 	if err != nil {
+	// 		return zero, zero, fmt.Errorf("prow job %s #%s had invalid 'completionTime': %s", job.Spec.Job, job.Status.BuildID, err)
+	// 	}
+	// }
+
+	// return started, finished, nil
+}
+
 func (o *options) handleJobs(w http.ResponseWriter, req *http.Request) {
-	if o.DeckURI == "" {
+	if o.jobLister == nil {
 		http.Error(w, "Unable to serve jobs data because no Deck URI was configured.", http.StatusInternalServerError)
 		return
 	}
 
-	if o.Interval.Seconds() == 0 {
-		http.Error(w, "Unable to serve jobs data because no indexing interval was configured.", http.StatusInternalServerError)
+	jobs, err := o.jobLister.List(labels.Everything())
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to load jobs: %v", err), http.StatusInternalServerError)
 		return
 	}
-
-	if jobBytes == nil {
-		w.Header().Set("Retry-After", fmt.Sprintf("%d", int(o.Interval.Seconds()))) // https://tools.ietf.org/html/rfc7231#section-7.1.3
-		http.Error(w, "Unable to serve jobs data because we have not fetched it yet.", http.StatusServiceUnavailable)
+	list := prow.JobList{Items: make([]prow.Job, 0, len(jobs))}
+	for _, job := range jobs {
+		list.Items = append(list.Items, *job)
+	}
+	data, err := json.Marshal(list)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to write jobs: %v", err), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-
-	jobLock.Lock()
-	defer jobLock.Unlock()
 	writer := encodedWriter(w, req)
 	defer writer.Close()
-
-	if _, err := writer.Write(jobBytes); err != nil {
+	if _, err := writer.Write(data); err != nil {
 		klog.Errorf("Failed to write response: %v", err)
 	}
 }
 
-func getJobs() ([]ProwJob, error) {
-	jobLock.Lock()
-	defer jobLock.Unlock()
-	var jobs ProwJobs
-	err := json.Unmarshal(jobBytes, &jobs)
-	if err == nil {
-		klog.Infof("Loaded %d prow jobs", len(jobs.Items))
-	} else {
-		klog.Infof("Failed to load prow jobs: %v", err)
-	}
-	return jobs.Items, err
+func getJobs() ([]Job, error) {
+	return nil, nil
 }

prow/index.go
@@ -1,0 +1,223 @@
+package prow
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
+)
+
+type DiskStore struct {
+	base   string
+	maxAge time.Duration
+	queue  workqueue.RateLimitingInterface
+	client *storage.Client
+}
+
+func NewDiskStore(client *storage.Client, path string, maxAge time.Duration) *DiskStore {
+	rate := workqueue.NewItemExponentialFailureRateLimiter(time.Minute, 30*time.Minute)
+	queue := workqueue.NewRateLimitingQueue(rate)
+	return &DiskStore{
+		base:   path,
+		maxAge: maxAge,
+		queue:  queue,
+		client: client,
+	}
+}
+
+type PathNotifier interface {
+	Notify(paths []string)
+}
+
+func (s *DiskStore) Run(ctx context.Context, informer cache.SharedIndexInformer, lister *Lister, notifier PathNotifier, workers int) {
+	informer.AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: func(obj interface{}) bool {
+			job, ok := obj.(*Job)
+			if !ok {
+				return false
+			}
+			switch job.Status.State {
+			case "aborted", "error", "failure":
+				if len(job.Status.URL) == 0 || job.Status.CompletionTime.IsZero() {
+					return false
+				}
+				if s.maxAge > 0 && job.Status.CompletionTime.Time.Add(s.maxAge).Before(time.Now()) {
+					return false
+				}
+				return true
+			default:
+				return false
+			}
+		},
+		Handler: cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				job, ok := obj.(*Job)
+				if !ok {
+					return
+				}
+				s.notifyChanged(fmt.Sprintf("%s/%s", job.Namespace, job.Name))
+			},
+		},
+	})
+
+	for i := 0; i < workers; i++ {
+		go func(i int) {
+			defer klog.V(2).Infof("Prow disk worker %d exited", i)
+			wait.UntilWithContext(ctx, func(ctx context.Context) {
+				for {
+					obj, done := s.queue.Get()
+					if done {
+						return
+					}
+					id, ok := obj.(string)
+					if !ok {
+						s.queue.Done(id)
+						klog.Errorf("unexpected id in queue: %v", obj)
+						continue
+					}
+					job, err := lister.Get(id)
+					if err != nil {
+						s.queue.Done(id)
+						klog.V(5).Infof("No job for %d: %v", id, err)
+						continue
+					}
+					ctx, cancelFn := context.WithTimeout(ctx, time.Minute)
+					func() {
+						defer cancelFn()
+						paths, err := s.write(ctx, job, notifier)
+						if err != nil {
+							if s.queue.NumRequeues(obj) > 5 {
+								s.queue.Forget(obj)
+							} else {
+								s.queue.AddRateLimited(obj)
+							}
+							s.queue.Done(obj)
+							klog.Errorf("failed to write job: %v", err)
+							return
+						}
+						notifier.Notify(paths)
+						s.queue.Done(id)
+					}()
+				}
+			}, time.Second)
+		}(i)
+	}
+	<-ctx.Done()
+}
+
+func (s *DiskStore) write(ctx context.Context, job *Job, notifier PathNotifier) ([]string, error) {
+	if job.Status.State == "error" && job.Status.URL == "https://github.com/kubernetes/test-infra/issues" {
+		return nil, nil
+	}
+	// path := s.pathForJob(job)
+	// klog.Infof("Should create directory %s", path)
+	u, err := url.Parse(job.Status.URL)
+	if err != nil {
+		return nil, fmt.Errorf("job %s has no valid status URL: %v", job.Name, err)
+	}
+	parts := strings.Split(strings.Trim(u.Path, "/"), "/")
+	if len(parts) < 5 {
+		return nil, fmt.Errorf("job %s URL is not valid: %s", job.Name, job.Status.URL)
+	}
+	if parts[0] != "view" || parts[1] != "gcs" {
+		return nil, fmt.Errorf("job %s URL is not valid: %s", job.Name, job.Status.URL)
+	}
+
+	bucket := parts[2]
+	switch trigger := parts[3]; trigger {
+	case "logs":
+		parts = parts[3:]
+		switch {
+		case len(parts) == 3:
+		case len(parts) == 4:
+		default:
+			return nil, fmt.Errorf("unrecognized logs path %q in %s", strings.Join(parts, "/"), job.Status.URL)
+		}
+		if _, err := strconv.Atoi(parts[len(parts)-1]); err != nil {
+			return nil, fmt.Errorf("unrecognized logs path %q in %s", strings.Join(parts, "/"), job.Status.URL)
+		}
+	case "pr-logs":
+		parts = parts[3:]
+		switch {
+		case len(parts) == 6 && parts[1] == "pull":
+		case len(parts) == 5 && parts[1] == "pull" && parts[2] == "batch":
+		case len(parts) == 5 && parts[1] == "pull":
+			if _, err := strconv.Atoi(parts[2]); err != nil {
+				return nil, fmt.Errorf("unrecognized pr-logs path %q in %s", strings.Join(parts, "/"), job.Status.URL)
+			}
+		default:
+			return nil, fmt.Errorf("unrecognized pr-logs path %q in %s", strings.Join(parts, "/"), job.Status.URL)
+		}
+		if _, err := strconv.Atoi(parts[len(parts)-1]); err != nil {
+			return nil, fmt.Errorf("unrecognized logs path %q in %s", strings.Join(parts, "/"), job.Status.URL)
+		}
+	default:
+		return nil, fmt.Errorf("unrecognized job prefix type %q in %s", parts[3], job.Status.URL)
+	}
+
+	build := Build{
+		Bucket:     s.client.Bucket(bucket),
+		Context:    ctx,
+		BucketPath: bucket,
+		Prefix:     path.Join(parts...) + "/",
+	}
+	start := time.Now()
+	accumulator, stale := NewAccumulator(s.base, &build, job.Status.CompletionTime.Time)
+	if !stale {
+		klog.V(7).Infof("Job %s is up to date", job.Status.URL)
+		return nil, nil
+	}
+	if err := ReadBuild(build, accumulator); err != nil {
+		klog.Infof("Download %s failed in %s: %v", job.Status.URL, time.Now().Sub(start).Truncate(time.Millisecond), err)
+		return nil, err
+	}
+	if err := accumulator.MarkCompleted(job.Status.CompletionTime.Time); err != nil {
+		klog.Errorf("Unable to mark job as completed: %v", err)
+	}
+	klog.Infof("Download %s succeeded in %s (%s)", job.Status.URL, time.Now().Sub(start).Truncate(time.Millisecond), job.Status.CompletionTime)
+	return nil, nil
+}
+
+func (s *DiskStore) pathForJob(job *Job) string {
+	return filepath.Join(s.base, job.Spec.Job, job.Status.BuildID)
+}
+
+func (s *DiskStore) notifyChanged(id string) {
+	s.queue.Add(id)
+}
+
+func (s *DiskStore) Sync() error {
+	start := time.Now()
+	mustExpire := s.maxAge != 0
+	expiredAt := start.Add(-s.maxAge)
+
+	return filepath.Walk(s.base, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			if os.IsNotExist(err) {
+				return nil
+			}
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+
+		if mustExpire && expiredAt.After(info.ModTime()) {
+			os.Remove(path)
+			klog.V(5).Infof("File expired: %s", path)
+			return nil
+		}
+		return nil
+	})
+}

prow/types.go
@@ -1,0 +1,51 @@
+package prow
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type JobList struct {
+	metav1.TypeMeta
+	metav1.ListMeta
+
+	Items []Job `json:"items"`
+}
+
+type Job struct {
+	metav1.TypeMeta
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec   JobSpec   `json:"spec"`
+	Status JobStatus `json:"status"`
+}
+
+type JobSpec struct {
+	Type string `json:"type"`
+	Job  string `json:"job"`
+}
+
+type JobStatus struct {
+	State          string      `json:"state"`
+	StartTime      metav1.Time `json:"startTime"`
+	CompletionTime metav1.Time `json:"completionTime"`
+	URL            string      `json:"url"`
+	BuildID        string      `json:"build_id"`
+}
+
+func (j Job) DeepCopyObject() runtime.Object {
+	copied := j
+	copied.ObjectMeta = *j.ObjectMeta.DeepCopy()
+	return &copied
+}
+
+func (j *JobList) DeepCopyObject() runtime.Object {
+	copied := *j
+	if j.Items != nil {
+		copied.Items = make([]Job, len(j.Items))
+		for i := range j.Items {
+			copied.Items[i] = *j.Items[i].DeepCopyObject().(*Job)
+		}
+	}
+	return &copied
+}

Dockerfile
@@ -5,11 +5,10 @@ RUN make build
 
 FROM centos:7
 COPY --from=0 /go/src/github.com/openshift/ci-search/search /usr/bin/
-COPY --from=0 /go/src/github.com/openshift/ci-search/build-indexer /usr/bin/
 RUN curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep-0.10.0-x86_64-unknown-linux-musl.tar.gz | \
     tar xvzf - --wildcards --no-same-owner --strip-components=1  -C /usr/bin '*/rg'
 RUN mkdir /var/lib/ci-search && chown 1000:1000 /var/lib/ci-search && chmod 1777 /var/lib/ci-search
 USER 1000:1000
 ENTRYPOINT ["search"]
-CMD ["--interval=5m", "--path=/var/lib/ci-search/", "--deck-uri=https://prow.svc.ci.openshift.org"]
+CMD ["--path=/var/lib/ci-search/", "--deck-uri=https://prow.svc.ci.openshift.org"]
 EXPOSE 8080

Makefile
@@ -1,4 +1,3 @@
 build:
-	go build -mod vendor ./testgrid/cmd/build-indexer
 	go build -mod vendor ./cmd/search
 .PHONY: build

bugzilla/comments.go
@@ -158,6 +158,7 @@ func (s *CommentStore) run(ctx context.Context, persisted PersistentCommentStore
 			if done {
 				return ctx.Err()
 			}
+			s.queue.Done(k)
 			id, err := strconv.Atoi(k.(string))
 			if err != nil {
 				klog.Warningf("comment id was not parsable to int: %v", err)

bugzilla/commentsdisk.go
@@ -47,14 +47,17 @@ func (s *CommentDiskStore) Run(ctx context.Context, lister *BugLister, store Com
 			}
 			id, err := strconv.Atoi(obj.(string))
 			if err != nil {
+				s.queue.Done(obj)
 				klog.Errorf("unexpected id in queue: %v", err)
 				continue
 			}
 			comments, ok := store.Get(id)
 			if !ok {
+				s.queue.Done(obj)
 				klog.V(5).Infof("No comments for %d", id)
 				continue
 			}
+			s.queue.Done(obj)
 			bug, err := lister.Get(id)
 			if err != nil {
 				klog.V(5).Infof("No bug for %d, defaulting", id)

cmd/search/grep.go
@@ -20,10 +20,16 @@ type CommandGenerator interface {
 	PathPrefix() string
 }
 
+type RipgrepSourceArguments interface {
+	// SearchPaths searches for paths matching the index's SearchType
+	// and MaxAge, and returns them as a slice of filesystem paths.
+	RipgrepSourceArguments(*Index, []string) ([]string, error)
+}
+
 type ripgrepGenerator struct {
-	execPath     string
-	searchPath   string
-	dynamicPaths PathSearcher
+	execPath   string
+	searchPath string
+	arguments  RipgrepSourceArguments
 }
 
 func (g ripgrepGenerator) Command(index *Index, search string) (string, []string, error) {
@@ -34,54 +40,23 @@ func (g ripgrepGenerator) Command(index *Index, search string) (string, []string
 		args = append(args, "--context", "0")
 	}
 	args = append(args, search)
-	var err error
-	args, err = g.dynamicPaths.SearchPaths(index, args)
+	newArgs, err := g.arguments.RipgrepSourceArguments(index, args)
 	if err != nil {
 		return "", nil, err
 	}
-	return g.execPath, args, nil
+	return g.execPath, newArgs, nil
 }
 
 func (g ripgrepGenerator) PathPrefix() string {
 	return g.searchPath
 }
 
-type grepGenerator struct {
-	execPath     string
-	searchPath   string
-	dynamicPaths PathSearcher
-}
-
-func (g grepGenerator) Command(index *Index, search string) (string, []string, error) {
-	args := []string{g.execPath, "--color=never", "-R", "--null", "-a"}
-	if index.Context >= 0 {
-		args = append(args, "--context", strconv.Itoa(index.Context))
-	} else {
-		args = append(args, "--context", "0")
-	}
-	args = append(args, search)
-	var err error
-	args, err = g.dynamicPaths.SearchPaths(index, args)
-	if err != nil {
-		return "", nil, err
-	}
-	return g.execPath, args, nil
-}
-
-func (g grepGenerator) PathPrefix() string {
-	return g.searchPath
-}
-
-func NewCommandGenerator(searchPath string, paths PathSearcher) (CommandGenerator, error) {
+func NewCommandGenerator(searchPath string, arguments RipgrepSourceArguments) (CommandGenerator, error) {
 	if path, err := exec.LookPath("rg"); err == nil {
 		klog.Infof("Using ripgrep at %s for searches", path)
-		return ripgrepGenerator{execPath: path, searchPath: searchPath, dynamicPaths: paths}, nil
+		return ripgrepGenerator{execPath: path, searchPath: searchPath, arguments: arguments}, nil
 	}
-	if path, err := exec.LookPath("grep"); err == nil {
-		klog.Infof("Using grep at %s for searches", path)
-		return grepGenerator{execPath: path, searchPath: searchPath, dynamicPaths: paths}, nil
-	}
-	return nil, fmt.Errorf("could not find 'rg' or 'grep' on the path")
+	return nil, fmt.Errorf("could not find 'rg' on the path")
 }
 
 // executeGrep search for matches to index and, for each match found,

commit 4323992d1bbea512e2c411c937af0eff8fc2c39b

prow/types: Make completionTime omitempty
To match [1], fixing a regression introduced by 06c45ec87f (Switch to
indexing jobs based on a list of input prow jobs, 2020-03-28, #44).
This fixes the durations for the pending jobs.  The current JavaScript
in cmd/search/httpchart.go has:

  if (job.status.completionTime === undefined) {
    job.finished = now;
  }  else {
    job.finished = isoParse(job.status.completionTime);
  }

so for pending jobs to get the 'now' default, they must have
completionTime unset.

[1]: https://github.com/kubernetes/test-infra/blob/e35a6bca1f57be69cbe0a24b0b6c076407b2e01f/prow/apis/prowjobs/v1/types.go#L624-L625

cmd/search/httpchartpng.go
@@ -79,7 +79,7 @@ func (o *options) handleChartPNG(w http.ResponseWriter, req *http.Request) {
 			continue
 		}
 
-		start, stop := job.Status.StartTime.Time, job.Status.CompletionTime.Time
+		start, stopPtr := job.Status.StartTime.Time, job.Status.CompletionTime
 		if start.Before(minTime) {
 			continue
 		}
@@ -122,8 +122,9 @@ func (o *options) handleChartPNG(w http.ResponseWriter, req *http.Request) {
 			}
 		}
 
-		if stop.IsZero() {
-			stop = maxTime
+		stop := maxTime
+		if stopPtr != nil {
+			stop = stopPtr.Time
 		}
 
 		dur := int(stop.Sub(start).Seconds())

cmd/search/jobs.go
@@ -24,17 +24,17 @@ func (o *options) handleJobs(w http.ResponseWriter, req *http.Request) {
 	}
 	// sort uncompleted -> newest completed -> oldest completed
 	sort.Slice(jobs, func(i, j int) bool {
-		iTime, jTime := jobs[i].Status.CompletionTime.Time, jobs[j].Status.CompletionTime.Time
-		if iTime.Equal(jTime) {
+		iTime, jTime := jobs[i].Status.CompletionTime, jobs[j].Status.CompletionTime
+		if iTime == nil {
 			return true
 		}
-		if iTime.IsZero() && !jTime.IsZero() {
-			return true
-		}
-		if !iTime.IsZero() && jTime.IsZero() {
+		if jTime == nil {
 			return false
 		}
-		return jTime.Before(iTime)
+		if iTime.Time.Equal(jTime.Time) {
+			return true
+		}
+		return jTime.Time.Before(iTime.Time)
 	})
 	list := prow.JobList{Items: make([]prow.Job, 0, len(jobs))}
 	for _, job := range jobs {

prow/index.go
@@ -104,7 +104,7 @@ func (s *DiskStore) Handler() cache.ResourceEventHandler {
 			}
 			switch job.Status.State {
 			case "aborted", "error", "failure", "success":
-				if len(job.Status.URL) == 0 || job.Status.CompletionTime.IsZero() {
+				if len(job.Status.URL) == 0 || job.Status.CompletionTime == nil {
 					return false
 				}
 				if s.maxAge > 0 && job.Status.CompletionTime.Time.Add(s.maxAge).Before(time.Now()) {

prow/indexgcs.go
@@ -112,7 +112,7 @@ func (r *IndexReader) Run(ctx context.Context, handler cache.ResourceEventHandle
 				Status: JobStatus{
 					State:          state,
 					URL:            statusURL.String(),
-					CompletionTime: metav1.Time{time.Unix(completed, 0)},
+					CompletionTime: &metav1.Time{time.Unix(completed, 0)},
 				},
 			}
 			r.add(job)

prow/types.go
@@ -26,11 +26,11 @@ type JobSpec struct {
 }
 
 type JobStatus struct {
-	State          string      `json:"state"`
-	StartTime      metav1.Time `json:"startTime"`
-	CompletionTime metav1.Time `json:"completionTime"`
-	URL            string      `json:"url"`
-	BuildID        string      `json:"build_id"`
+	State          string       `json:"state"`
+	StartTime      metav1.Time  `json:"startTime"`
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+	URL            string       `json:"url"`
+	BuildID        string       `json:"build_id"`
 }
 
 func (j Job) DeepCopyObject() runtime.Object {